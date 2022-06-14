f5aa1b07300becac6fe690f0e0ac7e5d0a77eb84
tunbosunxx <104427420+tunbosunxx@users.noreply.github.com>
2022-06-14 14:42:11 +0200

    Add files via upload

---
+++ I4G006278OF7/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class  post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+
+    
+
+
